59f395c37d4dca3604aa5d2a7d6524a08b0d758b
ShardulNalegave <nalegaveshardul40@gmail.com>
2018-06-26 17:36:35 +0530

    Added Syntax Highlight For Python

--- main.py
+++ main.py
@@ -32,6 +32,32 @@ import os.path as path
 import os
 import wx
 import wx.stc as stc
+import keyword
+
+if wx.Platform == '__WXMSW__':
+    faces = {'times': 'Times New Roman',
+             'mono': 'Courier New',
+             'helv': 'Arial',
+             'other': 'Comic Sans MS',
+             'size': 10,
+             'size2': 8,
+             }
+elif wx.Platform == '__WXMAC__':
+    faces = {'times': 'Times New Roman',
+             'mono': 'Monaco',
+             'helv': 'Arial',
+             'other': 'Comic Sans MS',
+             'size': 12,
+             'size2': 10,
+             }
+else:
+    faces = {'times': 'Times',
+             'mono': 'Courier',
+             'helv': 'Helvetica',
+             'other': 'new century schoolbook',
+             'size': 12,
+             'size2': 10,
+             }
 
 
 class App(wx.Frame):
@@ -65,8 +91,13 @@ class App(wx.Frame):
         quit_menuitem = filemenu.Append(
             wx.ID_EXIT, "&Quit\tCtrl+Q", "Quits The Application")
 
+        projectmenu = wx.Menu()
+        project_install_package = projectmenu.Append(
+            wx.ID_ANY, "&Install A Package\tCtrl+Shift+I", "Install A Python Package")
+
         menubar = wx.MenuBar()
         menubar.Append(filemenu, "&File")
+        menubar.Append(projectmenu, "&Project")
         self.SetMenuBar(menubar)
 
         self.Bind(wx.EVT_MENU, self.new_file, file_new)
@@ -77,6 +108,9 @@ class App(wx.Frame):
 
         self.CreateStatusBar()
         self.StatusBar.SetBackgroundColour((220, 220, 220))
+
+    def install_package(self, event):
+        pass
 
     def load_widgets(self):
 
@@ -89,18 +123,125 @@ class App(wx.Frame):
         self.editor.SetViewWhiteSpace(False)
         self.editor.SetMargins(10, 0)
         self.editor.SetMarginType(1, stc.STC_MARGIN_NUMBER)
-        self.editor.SetMarginWidth(1, 25)
+        self.editor.SetMarginWidth(2, 35)
+        # self.editor.SetMarginType(
+        #     2, stc.STC_MARGIN_SYMBOL | stc.STC_MARGIN_NUMBER)
+        # self.editor.SetMarginMask(2, stc.STC_MASK_FOLDERS)
+        # self.editor.SetMarginSensitive(2, True)
+        # self.editor.SetMarginWidth(2, 35)
+        self.editor.SetLexer(stc.STC_LEX_PYTHON)
+        self.editor.SetKeyWords(
+            0, " ".join(keyword.kwlist))
+        self.editor.SetProperty("fold", "1")
+        self.editor.SetProperty("tab.timmy.whinge.level", "1")
+        self.editor.SetEdgeMode(stc.STC_EDGE_BACKGROUND)
+        self.editor.SetEdgeColumn(78)
         self.editor.SetValue("""
 
 from sys import argv
 
 def greet(name):
-    print("Hello, {}".format(name))
+        print("Hello, {}".format(name))
 
 if __name__ == "__main__":
-    greet(argv[1])
+        greet(argv[1])
 
 """)
+
+        self.syntax_highlight_styles_python = {
+            "default": "#000000",
+            "keyword": "#EF5350",
+            "comment-line": "#BDBDBD",
+            "block_comment": "#BDBDBD",
+            "string": "#43A047",
+            "block_string": "#43A047",
+            "operators": "#EF5350",
+            "number": "#29B6F6",
+            "EOL_when_string_not_closed": "#E53935",
+            "class_and_function_names": "#7E57C2",
+            "identifiers": "#000000"
+        }
+
+        self.global_styles = {
+            "linenum_back": "#F5F5F5",
+            "cursor": "#000000"
+        }
+
+        self.update_syntax_highlight(
+            self.syntax_highlight_styles_python, self.global_styles)
+
+    def update_syntax_highlight(self, python_styles, global_styles):
+
+        if not isinstance(python_styles, dict):
+            print("Styles Passed Should Be Dictionaries")
+            return False
+
+        # Make some styles,  The lexer defines what each style is used for, we
+        # just have to define what each style looks like.  This set is adapted from
+        # Scintilla sample property files.
+
+        # Global default styles for all languages
+        self.editor.StyleSetSpec(stc.STC_STYLE_DEFAULT,
+                                 "face:%(helv)s,size:%(size)d" % faces)
+        self.editor.StyleClearAll()  # Reset all to be like the default
+
+        # Global default styles for all languages
+        self.editor.StyleSetSpec(stc.STC_STYLE_DEFAULT,
+                                 "face:%(helv)s,size:%(size)d" % faces)
+        self.editor.StyleSetSpec(stc.STC_STYLE_LINENUMBER,
+                                 "back:" + global_styles["linenum_back"] + ",face:%(helv)s,size:%(size2)d" % faces)
+        self.editor.StyleSetSpec(
+            stc.STC_STYLE_CONTROLCHAR, "face:%(other)s" % faces)
+        self.editor.StyleSetSpec(stc.STC_STYLE_BRACELIGHT,
+                                 "fore:#FFFFFF,back:#0000FF,bold")
+        self.editor.StyleSetSpec(stc.STC_STYLE_BRACEBAD,
+                                 "fore:#000000,back:#FF0000,bold")
+
+        # Python styles
+        # Default
+        self.editor.StyleSetSpec(stc.STC_P_DEFAULT,
+                                 "fore:" + python_styles["default"] + ",face:%(helv)s,size:%(size)d" % faces)
+        # Comments
+        self.editor.StyleSetSpec(stc.STC_P_COMMENTLINE,
+                                 "fore:" + python_styles["comment-line"] + ",face:%(other)s,size:%(size)d" % faces)
+        # Number
+        self.editor.StyleSetSpec(stc.STC_P_NUMBER,
+                                 "fore:" + python_styles["number"] + ",size:%(size)d" % faces)
+        # String
+        self.editor.StyleSetSpec(stc.STC_P_STRING,
+                                 "fore:" + python_styles["string"] + ",face:%(helv)s,size:%(size)d" % faces)
+        # Single quoted string
+        self.editor.StyleSetSpec(stc.STC_P_CHARACTER,
+                                 "fore:" + python_styles["string"] + ",face:%(helv)s,size:%(size)d" % faces)
+        # Keyword
+        self.editor.StyleSetSpec(
+            stc.STC_P_WORD, "fore:" + python_styles["keyword"] + ",size:%(size)d" % faces)
+        # Triple quotes
+        self.editor.StyleSetSpec(stc.STC_P_TRIPLE,
+                                 "fore:" + python_styles["block_string"] + ",size:%(size)d" % faces)
+        # Triple double quotes
+        self.editor.StyleSetSpec(stc.STC_P_TRIPLEDOUBLE,
+                                 "fore:" + python_styles["block_string"] + ",size:%(size)d" % faces)
+        # Class name definition
+        self.editor.StyleSetSpec(stc.STC_P_CLASSNAME,
+                                 "fore:" + python_styles["class_and_function_names"] + ",size:%(size)d" % faces)
+        # Function or method name definition
+        self.editor.StyleSetSpec(stc.STC_P_DEFNAME,
+                                 "fore:" + python_styles["class_and_function_names"] + ",size:%(size)d" % faces)
+        # Operators
+        self.editor.StyleSetSpec(
+            stc.STC_P_OPERATOR, "fore:" + python_styles["operators"] + ",size:%(size)d" % faces)
+        # Identifiers
+        self.editor.StyleSetSpec(stc.STC_P_IDENTIFIER,
+                                 "fore:" + python_styles["identifiers"] + ",face:%(helv)s,size:%(size)d" % faces)
+        # Comment-blocks
+        self.editor.StyleSetSpec(stc.STC_P_COMMENTBLOCK,
+                                 "fore:" + python_styles["block_comment"] + ",size:%(size)d" % faces)
+        # End of line where string is not closed
+        self.editor.StyleSetSpec(
+            stc.STC_P_STRINGEOL, "fore:" + python_styles["EOL_when_string_not_closed"] + ",face:%(mono)s,back:#E0C0E0,eol,size:%(size)d" % faces)
+
+        self.editor.SetCaretForeground(global_styles["cursor"])
 
     def new_file(self, event):
         projectTypeList = [
@@ -119,10 +260,10 @@ if __name__ == "__main__":
 from sys import argv
 
 def greet(name):
-    print("Hello, {}".format(name))
+        print("Hello, {}".format(name))
 
 if __name__ == "__main__":
-    greet(argv[1])
+        greet(argv[1])
 
 """)
             elif selection == "Tkinter App":
@@ -163,10 +304,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+        return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
 
 """)
         dlg.Destroy()
